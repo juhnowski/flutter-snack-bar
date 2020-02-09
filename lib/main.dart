dd64b09cead4047c4d2d2896ed3fbc94ccfa3046
Juhnowski <juhnowski@gmail.com>
2020-02-09 10:40:18 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+// ...
+
+void main() => runApp(MyApp());
+
+// ...
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Code Sample for Scaffold.of.',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: MyScaffoldBody(),
+        appBar: AppBar(title: Text('Scaffold.of Example')),
+      ),
+      color: Colors.white,
+    );
+  }
+}
+
+// ...
+
+class MyScaffoldBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('SHOW A SNACKBAR'),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Have a snack!'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
